5a74a29aeb06fa7f4a7bcf2ca565412406936586
田鹏 <javatp@163.com>
2020-01-14 19:45:25 +0800

    登录路径映射变更

--- src/main/java/com/tp/controller/LoginController.java
+++ src/main/java/com/tp/controller/LoginController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
-    @RequestMapping({"", "/"})
+    @RequestMapping("")
     public String index(){
         return "index";
     }
